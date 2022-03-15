01d81cd3bbff784adb5a779df545817a9954acb1
Mark Chuang <markchuangth@gmail.com>
2022-03-14 22:10:54 -0400

    small changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  // res.send('Hello World! This is Mark. Canada #1. Taiwan #1')
+  // res.send('Hello World! This is Mark. Canada #1. Taiwan #1. China #1.')
   res.render('index')
 })
 
